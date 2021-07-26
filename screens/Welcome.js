b4a2dea26c07abbef7fe0ce287c2fa19828882c2
matmang <64837797+matmang@users.noreply.github.com>
2021-07-26 12:53:55 +0900

    Update Welcome.js

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Button } from "react-native";
 
 export default ({navigation}) => (
   <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
     <Text>Welcome!</Text>
+    <Button onPress={() => navigation.navigate("Sign Up")} title={"Sign Up"}/>
+    <Button onPress={() => navigation.navigate("Sign In")} title={"Sign In"}/>
   </View>
 );
